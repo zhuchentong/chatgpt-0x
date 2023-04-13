b961c0644cce8366f4c111d3f53e5ca7f0b77fb7
zhuchentong <2037630@163.com>
2023-04-13 10:16:29 +0800

    fix: update dockerfile

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
 import { ConfigModule } from '@nestjs/config'
 import { DatabaseModule } from './database/database.module'
 import { RedisModule } from './redis/redis.module'
@@ -38,7 +37,6 @@ const environment = process.env.NODE_ENV || 'development'
       },
     ]),
   ],
-  controllers: [AppController],
   providers: [
     {
       provide: APP_INTERCEPTOR,
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,14 +2,14 @@ module.exports = {
   deploy: {
     production: {
       'user': 'root', // ssh 登陆服务器用户名
-      'host': '121.5.165.149', // ssh 地址服务器domain/IP
+      'host': '121.37.181.45', // ssh 地址服务器domain/IP
       'ref': 'origin/master', // Git远程/分支
-      'repo': 'git@github.com:gopowerteam-mall/mall-service.git', // git地址使用ssh地址
-      'path': '/home/apps/mall-service', // 项目存放服务器文件路径
+      'repo': 'git@github.com:zhuchentong/chatgpt-0x.git', // git地址使用ssh地址
+      'path': '/data/chatgpt', // 项目存放服务器文件路径
       'ssh_options': 'StrictHostKeyChecking=no',
       'pre-deploy': 'git fetch --all',
       'post-deploy':
-        'docker build -t chatgpt-0x . && docker rm -f chatgpt-0x && docker run -d -p 4000:4000 chatgpt-0x', // 部署后的动作
+        'docker build -t chatgpt-0x . && docker rm -f chatgpt-0x && docker run -d -p 4000:4000 --name chatgpt-0x -v /data/chatgpt/.env.production:/app/apps/server/.env.production chatgpt', // 部署后的动作
     },
   },
 }
