e816c9ccb66c86f031428ddefd00f7dd8727bf2b
zhuchentong <2037630@163.com>
2023-04-17 13:13:36 +0800

    feat(custom): update request

---
+++ apps/admin/src/http/models/LoginInput.ts
@@ -0,0 +1,14 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type LoginInput = {
+  /**
+   * 用户名
+   */
+  username: string
+  /**
+   * 密码
+   */
+  password: string
+}
---
+++ apps/admin/src/http/services/WechatService.ts
@@ -0,0 +1,111 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import {
+  RequestService,
+  RequestGenerateType,
+  type RequestSendOptions,
+  type RequestPlugin,
+  type RequestGenerateOptions,
+} from '@gopowerteam/request'
+export class WechatService {
+  // 请求实例
+  private request = RequestService.getInstance()
+  private service = ''
+
+  private generateRequest(
+    requestSendOptions: RequestSendOptions,
+    requestPlugins: RequestPlugin[] = [],
+    requestGenerateOptions?: RequestGenerateOptions,
+  ) {
+    switch (true) {
+      case requestGenerateOptions?.type === RequestGenerateType.URL:
+        // 生成URL
+        return this.request.toURL(requestSendOptions, requestPlugins)
+      default: {
+        // 请求数据
+        const result = this.request.send(requestSendOptions, requestPlugins)
+
+        return result
+      }
+    }
+  }
+
+  public checkSignature(
+    requestQuery: RequestQueryParams.CheckSignature,
+    requestPlugins: RequestPlugin[],
+    requestGenerateOptions: RequestGenerateOptions & {
+      type: RequestGenerateType.URL
+    },
+  ): string
+  public checkSignature(
+    requestQuery: RequestQueryParams.CheckSignature,
+    requestPlugins?: RequestPlugin[],
+    requestGenerateOptions?: RequestGenerateOptions,
+  ): Promise<void>
+  public checkSignature(
+    requestQuery: RequestQueryParams.CheckSignature,
+    requestPlugins: RequestPlugin[] = [],
+    requestGenerateOptions?: RequestGenerateOptions,
+  ): Promise<void> | string {
+    const requestSendOptions = {
+      service: this.service,
+      path: '/api/wechat/message',
+      method: 'get',
+      paramsQuery: requestQuery,
+    }
+
+    return this.generateRequest(
+      requestSendOptions,
+      requestPlugins,
+      requestGenerateOptions,
+    )
+  }
+
+  public WXMPController_message(
+    requestQuery: RequestQueryParams.WXMPController_message,
+    requestPlugins: RequestPlugin[],
+    requestGenerateOptions: RequestGenerateOptions & {
+      type: RequestGenerateType.URL
+    },
+  ): string
+  public WXMPController_message(
+    requestQuery: RequestQueryParams.WXMPController_message,
+    requestPlugins?: RequestPlugin[],
+    requestGenerateOptions?: RequestGenerateOptions,
+  ): Promise<void>
+  public WXMPController_message(
+    requestQuery: RequestQueryParams.WXMPController_message,
+    requestPlugins: RequestPlugin[] = [],
+    requestGenerateOptions?: RequestGenerateOptions,
+  ): Promise<void> | string {
+    const requestSendOptions = {
+      service: this.service,
+      path: '/api/wechat/message',
+      method: 'post',
+      paramsQuery: requestQuery,
+    }
+
+    return this.generateRequest(
+      requestSendOptions,
+      requestPlugins,
+      requestGenerateOptions,
+    )
+  }
+}
+
+namespace RequestQueryParams {
+  export type CheckSignature = {
+    signature: string
+    timestamp: string
+    nonce: string
+    echostr: string
+  }
+  export type WXMPController_message = {
+    msg_signature: string
+    timestamp: string
+    nonce: string
+    openid: string
+    appId: string
+  }
+}
