121d7c2a36229a65417e788362d396993e157631
zhuchentong <2037630@163.com>
2023-04-18 11:24:30 +0800

    release: release

--- scripts/deploy/index.mjs
+++ scripts/deploy/index.mjs
@@ -2,7 +2,7 @@
 await $`rm -rf /www/wwwroot/chatgpt-0x.1zhizu.com`
 await $`mv /data/chatgpt/current/apps/web/dist /www/wwwroot/chatgpt-0x.1zhizu.com`
 
-await $`rm -rf /www/wwwroot/admin.chatgpt-0x.1zhizu.com`
+await $`rm -rf /www/wwwroot/admin-chatgpt-0x.1zhizu.com`
 await $`mv /data/chatgpt/current/apps/admin/dist /www/wwwroot/admin-chatgpt-0x.1zhizu.com`
 
 await $`pm2 reload chatgpt-0x-server`
