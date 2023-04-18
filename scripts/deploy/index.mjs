8b8fb2f7975826f194a9ad6d93646bcdae7ae602
zhuchentong <2037630@163.com>
2023-04-18 11:21:07 +0800

    release: release

--- scripts/deploy/index.mjs
+++ scripts/deploy/index.mjs
@@ -5,4 +5,4 @@ await $`mv /data/chatgpt/current/apps/web/dist /www/wwwroot/chatgpt-0x.1zhizu.co
 await $`rm -rf /www/wwwroot/admin.chatgpt-0x.1zhizu.com`
 await $`mv /data/chatgpt/current/apps/admin/dist /www/wwwroot/admin-chatgpt-0x.1zhizu.com`
 
-await $`pm2 reload chatgpt-server`
+await $`pm2 reload chatgpt-0x-server`
