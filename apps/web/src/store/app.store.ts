8acab18329edbaa8a6c3cabce16f40ce931f29fc
zhuchentong <2037630@163.com>
2023-04-20 00:56:07 +0800

    fix: 修复警告信息

--- apps/web/src/store/app.store.ts
+++ apps/web/src/store/app.store.ts
@@ -1,23 +1,25 @@
 import { defineStore } from 'pinia'
+import type { Ref } from 'vue'
 
 interface State {
   // 系统准备状态
   ready: boolean
   // 页面标题
   title: string
+  desktop: Ref<boolean>
+  mobile: Ref<boolean>
 }
 
 const initialState: State = {
   ready: false,
   title: '',
+  desktop: useMediaQuery('(min-width: 768px)'),
+  mobile: useMediaQuery('(max-width: 768px)'),
 }
 
 export const useAppStore = defineStore('app', {
   state: () => initialState,
-  getters: {
-    desktop: () => useMediaQuery('(min-width: 768px)').value,
-    mobile: () => useMediaQuery('(max-width: 768px)').value,
-  },
+
   actions: {
     /**
      * 更新系统状态
