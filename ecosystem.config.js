21d03deebf68c7a89fd9e3853bc5659e451d980c
zhuchentong <2037630@163.com>
2023-04-20 18:34:13 +0800

    release: 优化日志输出

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,9 @@ module.exports = {
       port: '8040',
       cwd: '/data/chatgpt/current/apps/server',
       script: 'dist/main.js',
+      error_file: './logs/error.log', // 错误输出日志
+      out_file: './logs/out.log', // 日志
+      log_date_format: 'YYYY-MM-DD HH:mm Z', // 日期格式
       env: {
         NODE_ENV: 'production',
       },
