ca8ecf0e80768d73e46bb2de4a6ef890b2a2b177
zhuchentong <2037630@163.com>
2023-04-25 23:55:48 +0800

    fix: 更新用户管理

--- apps/admin/src/views/users/index.vue
+++ apps/admin/src/views/users/index.vue
@@ -2,18 +2,13 @@
   <PageContainer title="用户管理">
     <template #action>
       <ASpace>
-        <AButton
-          type="primary"
-          @click="onCreate">
-          创建
-        </AButton>
+        <AButton type="primary">创建</AButton>
       </ASpace>
     </template>
     <DataTable
       ref="table"
       action-align="right"
       :columns="columns"
-      :edit-forms="editsForms"
       :load-data="loadData"
       :pagination="pageService"
       row-key="username"></DataTable>
@@ -24,16 +19,19 @@
 
 <script setup lang="ts">
 import {
-  type FormItemsOptions,
   type LoadDataParams,
   type TableColumnsOptions,
   useTable,
 } from '@gopowerteam/vue-dynamic-table'
 import { useRequest } from 'virtual:request'
+import dayjs from 'dayjs'
+import minMax from 'dayjs/plugin/minMax'
 import { PageService } from '@/http/extends/page.service'
-import { EnableStateDict, ProductTypeDict } from '@/config/dict.config'
-import type { Product } from '@/http/models/Product'
+import { EnableStateDict } from '@/config/dict.config'
+import type { User } from '@/http/models/User'
+import { ProductType } from '@/config/enum.config'
 
+dayjs.extend(minMax)
 const pageService = new PageService()
 const table = $(useTable('table'))
 
@@ -42,79 +40,60 @@ const [productService, userService] = useRequest(
   (service) => service.UserService,
 )
 
-function onCreate() {
-  table.edit({
-    title: '创建',
-    columns: 1,
-    record: {
-      title: '',
-      type: '',
-      value: '',
-      price: '',
-    },
-    appendRowKey: true,
-    submit: (data: any) => {
-      productService
-        .createProduct({
-          ...data,
-          value: +data.value,
-          price: data.price * 100,
-        })
-        .then(() => {
-          table.reload()
-        })
-    },
-  })
-}
-
 function loadData({ update }: LoadDataParams) {
-  userService.findUsers([pageService]).then((data) => {
-    console.log(data)
-    // update(data)
+  userService.findUsers([pageService]).then(({ data }) => {
+    update(data)
   })
 }
 
-const editsForms: FormItemsOptions = [
+const columns: TableColumnsOptions<User> = [
   {
-    key: 'title',
-    title: '名称',
-    rules: [{ required: true, message: '请输入名称' }],
-    render: (r) => r.input(),
+    key: 'id',
+    title: 'ID',
   },
   {
-    key: 'type',
-    title: '产品类型',
-    rules: [{ required: true, message: '请选择产品类型' }],
+    key: 'email',
+    title: '邮箱',
+  },
+  {
+    key: 'mobile',
+    title: '手机号',
+  },
+  {
+    key: 'openid',
+    title: 'openid',
+  },
+  {
+    key: 'balanceCount',
+    title: '次数余额',
     render: (r) =>
-      r.select({
-        options: ProductTypeDict,
+      r.text({
+        text: (record) =>
+          `${record.balances
+            .filter((x) => x.type === ProductType.Count)
+            .reduce((r, v) => (r += v.currentCount), 0)
+            .toString()}`,
       }),
   },
   {
-    key: 'value',
-    title: '次数/天数',
-    render: (r) => r.input({ placeholder: '请输入次数/天数' }),
-  },
-  {
-    key: 'price',
-    title: '价格',
-    render: (r) => r.input(),
-  },
-]
+    key: 'balanceTime',
+    title: '时间余额',
+    render: (r) =>
+      r.text({
+        text: (record) => {
+          const endTime = dayjs.max(
+            record.balances
+              .filter((x) => x.type === ProductType.Time)
+              .map((x) => dayjs(x.endTime)),
+          )
 
-const columns: TableColumnsOptions<Product> = [
-  {
-    key: 'title',
-    title: '产品名称',
-  },
-  {
-    key: 'type',
-    title: '产品类型',
-    render: (r) => r.dict({ dict: ProductTypeDict }),
-  },
-  {
-    key: 'value',
-    title: '次数/天数',
+          if (endTime) {
+            return endTime.diff(dayjs()).toString()
+          } else {
+            return '0'
+          }
+        },
+      }),
   },
   {
     key: 'enable',
@@ -125,65 +104,9 @@ const columns: TableColumnsOptions<Product> = [
       }),
   },
   {
-    key: 'price',
-    title: '价格',
-    formatter: (record) => record.price / 100,
-    render: (r) => r.text({ text: (record) => `¥ ${record.price}` }),
-  },
-  {
-    key: 'action',
-    title: '操作',
-    render: (r) =>
-      r.button({
-        buttons: [
-          {
-            text: (record) => EnableStateDict.get(!record.enable) as string,
-            confirm: true,
-            confirmText: '是否确定执行此操作？',
-            callback: (record) => {
-              productService
-                .updateProduct(record.id, { enable: !record.enable })
-                .then(() => {
-                  table.reload()
-                })
-            },
-          },
-          // {
-          //   text: '删除',
-          //   confirm: true,
-          //   confirmText: '是否确定删除此产品？',
-          //   callback: (record) => {
-          //     productService.removeProduct(record.id).then(() => {
-          //       table.reload()
-          //     })
-          //   },
-          // },
-          {
-            text: '编辑',
-            callback: (record) => {
-              table.edit({
-                title: '编辑',
-                record: {
-                  ...record,
-                  price: record.price,
-                },
-                columns: 1,
-                submit: async (data) => {
-                  productService
-                    .updateProduct(record.id, {
-                      ...data,
-                      value: +data.value,
-                      price: data.price * 100,
-                    })
-                    .then(() => {
-                      table.reload()
-                    })
-                },
-              })
-            },
-          },
-        ],
-      }),
+    key: 'createdAt',
+    title: '创建时间',
+    render: (r) => r.date(),
   },
 ]
 </script>
