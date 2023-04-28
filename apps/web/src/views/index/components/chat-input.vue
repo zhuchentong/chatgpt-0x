68527c5e163f3aa5741ca7fd890fefc38e819853
zhuchentong <2037630@163.com>
2023-04-28 10:39:37 +0800

    release: 添加方向键恢复历史消息支持

--- apps/web/src/views/index/components/chat-input.vue
+++ apps/web/src/views/index/components/chat-input.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="chat-input p-5 space-y-2">
     <div class="toolbar flex"></div>
-    <div class="input-container flex items-end">
+    <div
+      class="input-container flex items-end"
+      @keydown.exact.delete="resetInputIndex"
+      @keydown.exact.down.stop.prevent="revertNextMessage"
+      @keydown.exact.up.stop.prevent="revertPreviousMessage">
       <n-input
         v-model:value="inputText"
         :autosize="{
@@ -11,6 +15,7 @@
         class="flex-auto"
         :placeholder="assistant?.placeholder || '请输入提问内容'"
         type="textarea"
+        @keypress="resetInputIndex"
         @keypress.exact.enter.stop.prevent="onSubmit" />
       <n-button
         class="w-50px h-35px"
@@ -30,14 +35,20 @@
 
 <script setup lang="ts">
 import { useChat } from '@/composables/use-chat'
+import { ChatRole } from '@/config/enum.config'
 import { useStore } from '@/store'
 
 const store = useStore()
 const { sendChatMessage, appendUserMessage } = useChat()
 
 let inputText = $ref('')
+let inputIndex = $ref<number | undefined>()
 const assistant = computed(() => store.chat.currentAssistant)
 const chat = $(computed(() => store.chat.currentChat))
+
+function resetInputIndex() {
+  inputIndex = undefined
+}
 
 /**
  * 发送消息
@@ -60,5 +71,52 @@ function onSubmit() {
   sendChatMessage(inputText as string)
   // 清空输入框
   inputText = ''
+}
+
+function revertPreviousMessage() {
+  if (inputText && inputIndex === undefined) {
+    return
+  }
+
+  const inputRecords = chat.records
+    .filter((record) => !record.deleted && record.role === ChatRole.User)
+    .filter((_, index) =>
+      inputIndex === undefined ? true : index < inputIndex,
+    )
+
+  if (inputIndex === undefined) {
+    inputIndex = inputRecords.length
+  }
+  inputIndex = inputRecords.length
+
+  const [lastInput] = inputRecords.reverse()
+
+  if (lastInput) {
+    inputText = lastInput.content
+    inputIndex--
+  }
+}
+
+function revertNextMessage() {
+  if (inputText && inputIndex === undefined) {
+    return
+  }
+
+  const inputRecords = chat.records
+    .filter((record) => !record.deleted && record.role === ChatRole.User)
+    .filter((_, index) =>
+      inputIndex === undefined ? true : index > inputIndex,
+    )
+
+  if (inputIndex === undefined) {
+    inputIndex = inputRecords.length
+  }
+
+  const [lastInput] = inputRecords
+
+  if (lastInput) {
+    inputText = lastInput.content
+    inputIndex++
+  }
 }
 </script>
