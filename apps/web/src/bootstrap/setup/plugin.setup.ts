edbc4a1ba8d24c0d1d78611b2e0f65ada876c7a9
zhuchentong <2037630@163.com>
2023-05-08 18:43:00 +0800

    release: release

--- apps/web/src/bootstrap/setup/plugin.setup.ts
+++ apps/web/src/bootstrap/setup/plugin.setup.ts
@@ -1,14 +1,5 @@
-import DynamicTable, { type DataRecord } from '@gopowerteam/vue-dynamic-table'
 import type { App } from 'vue'
 
-export default function (app: App) {
-  app.use(DynamicTable, {
-    name: 'data-table',
-    override: {
-      table: {
-        // 支持重写&自定义
-        test: () => (row: DataRecord) => h('div', [row.name]),
-      },
-    },
-  })
+export default function (_app: App) {
+  //
 }
