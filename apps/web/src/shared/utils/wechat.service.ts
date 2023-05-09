a4ceaf3624f1c30ca4c26418ac52c4d70d0da86e
zhuchentong <2037630@163.com>
2023-05-09 10:59:13 +0800

    release: release

--- apps/web/src/shared/utils/wechat.service.ts
+++ apps/web/src/shared/utils/wechat.service.ts
@@ -64,7 +64,7 @@ export class WechatService {
       wx.config({
         ...config,
         beta: true, // 必须这么写，否则wx.invoke调用形式的jsapi会有问题
-        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
         jsApiList: wechatApiList, // 必填，需要使用的JS接口列表，凡是要调用的接口都需要传进来
       })
     })
